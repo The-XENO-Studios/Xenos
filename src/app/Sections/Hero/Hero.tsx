508c7bc6025a22dd04ef3d2ddf9468de4345e03b
The-XENO-Studios <thexenostudios@gmail.com>
2023-12-11 15:33:48 +0600

    Huge Update : Notification

--- src/app/Sections/Hero/Hero.tsx
+++ src/app/Sections/Hero/Hero.tsx
@@ -3,7 +3,7 @@ import HeroImage from "./HeroImage";
 const Hero = () => {
   return (
     <section className="w-screen h-screen relative">
-      <div className="w-screen h-screen absolute z-50 flex justify-center items-center">
+      <div className="w-screen h-screen absolute z-10 flex justify-center items-center">
         <HeroImage />
       </div>
       <div className="canvas w-full">
